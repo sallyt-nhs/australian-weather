9425a7ea96433c40a32beefc77f80083b3de097f
Sally Thompson <sally.thompson37@nhs.net>
2022-06-23 14:31:03 +0100

    facet by wind direction

--- R/create-plot.R
+++ R/create-plot.R
@@ -17,7 +17,8 @@ df %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(colour = "red") +
   ggtitle("Penrith is a humid city, except when it's really hot") +
-  theme_minimal()
+  theme_minimal() + 
+  facet_wrap(~WindDir9am)
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
